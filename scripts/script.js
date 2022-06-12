34f81afdbdcc7b7aad27ec8647bef854ff5008fb
Nivas Reddy <srinivasreddysagi@gmail.com>
2022-06-12 20:08:09 +0530

    Added menu button like feature for nav bar in mobile view

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const menuButton = document.getElementById("menu");
+const menuList = document.getElementById("menu-list");
+const links = Array(
+    document.getElementById("item-1"),
+    document.getElementById("item-2"),
+    document.getElementById("item-3")
+);
+
+menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("show");
+    menuList.classList.toggle("show");
+});
+
+links.forEach((e) => {
+    e.addEventListener("click", () => {
+        menuButton.classList.remove("show");
+        menuList.classList.remove("show");
+    });
+});
